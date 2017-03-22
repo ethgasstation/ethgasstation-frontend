969e4b309832d9f31a9d5f411d0d1944f734de47
jake <jacobelkins@protonmail.com>
2017-03-22 15:26:59 -0500

    navbar buttonv2

--- index.php
+++ index.php
@@ -123,11 +123,27 @@
                 <a id="menu_toggle"><i class="fa fa-bars"></i></a>
               </div>
               <p class="navbar-text"><strong><?php echo "Estimates over last 10,000 blocks - Last update: Block <span style = 'color:#1ABB9C'> $latestblock" ?></strong></span></p>
-                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Select Currency<span class="caret"></span></button>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">EUR</a></li>
-                    <li><a href="#">GBP</a></li>
+
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/img.jpg" alt="">John Doe
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;"> Profile</a></li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="badge bg-red pull-right">50%</span>
+                        <span>Settings</span>
+                      </a>
+                    </li>
+                    <li><a href="javascript:;">Help</a></li>
+                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                   </ul>
+                </li>
+
+                
             </nav>
           </div>
          </div>
