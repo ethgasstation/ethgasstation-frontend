913e871f8d41e0c155ca19c5b0b900daa38567b4
jake <jacobelkins@protonmail.com>
2017-03-22 15:39:47 -0500

    navbar buttonv3

--- index.php
+++ index.php
@@ -127,19 +127,12 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">John Doe
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Profile</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Settings</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Help</a></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                    <li><a href="javascript:;"> EUR</a></li>
+                    <li><a href="javascript:;"> GBP</a></li>
+                    <li><a href="javascript:;"> CNY</a></li>
                   </ul>
                 </li>
 
