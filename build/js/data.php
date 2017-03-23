d3e7d0974d8efe30cd60a8396817b1d912cb0374
jake <jacobelkins@protonmail.com>
2017-03-23 09:17:52 -0500

    graph

--- build/js/data.php
+++ build/js/data.php
@@ -28,48 +28,79 @@ if ($row['medianMinedDelay'] == null)
 }
 
 $graphData = array();
-$query2 = "SELECT * FROM txDataLast100b ORDER BY id DESC LIMIT 11";
+$query2 = "SELECT * FROM txDataLast100b ORDER BY id DESC LIMIT 20";
 $result2 = $mysqli->query($query2);
 
 while ($row2 = $result2->fetch_assoc()){
 	array_push($graphData,$row2);
 }
 
-$x1 = $graphData[10]['latestblockNum'];
-$x2 = $graphData[9]['latestblockNum'];
-$x3 = $graphData[8]['latestblockNum'];
-$x4 = $graphData[7]['latestblockNum'];
-$x5 = $graphData[6]['latestblockNum'];
-$x6 = $graphData[5]['latestblockNum'];
-$x7 = $graphData[4]['latestblockNum'];
-$x8 = $graphData[3]['latestblockNum'];
-$x9 = $graphData[2]['latestblockNum'];
-$x10 = $graphData[1]['latestblockNum'];
-$x11 = $graphData[0]['latestblockNum'];
+$x1 = $graphData[19]['latestblockNum'];
+$x2 = $graphData[18]['latestblockNum'];
+$x3 = $graphData[17]['latestblockNum'];
+$x4 = $graphData[16]['latestblockNum'];
+$x5 = $graphData[15]['latestblockNum'];
+$x6 = $graphData[14]['latestblockNum'];
+$x7 = $graphData[13]['latestblockNum'];
+$x8 = $graphData[12]['latestblockNum'];
+$x9 = $graphData[11]['latestblockNum'];
+$x10 = $graphData[10]['latestblockNum'];
+$x11 = $graphData[9]['latestblockNum'];
+$x12 = $graphData[8]['latestblockNum'];
+$x13 = $graphData[7]['latestblockNum'];
+$x14 = $graphData[6]['latestblockNum'];
+$x15 = $graphData[5]['latestblockNum'];
+$x16 = $graphData[4]['latestblockNum'];
+$x17 = $graphData[3]['latestblockNum'];
+$x18 = $graphData[2]['latestblockNum'];
+$x19 = $graphData[1]['latestblockNum'];
+$x20 = $graphData[0]['latestblockNum'];
 
-$ya1 = round($graphData[10]['ethConsumedLast100']/1000000000,2);
-$ya2 = round($graphData[9]['ethConsumedLast100']/1000000000,2);
-$ya3 = round($graphData[8]['ethConsumedLast100']/1000000000,2);
-$ya4 = round($graphData[7]['ethConsumedLast100']/1000000000,2);
-$ya5 = round($graphData[6]['ethConsumedLast100']/1000000000,2);
-$ya6 = round($graphData[5]['ethConsumedLast100']/1000000000,2);
-$ya7 = round($graphData[4]['ethConsumedLast100']/1000000000,2);
-$ya8 = round($graphData[3]['ethConsumedLast100']/1000000000,2);
-$ya9 = round($graphData[2]['ethConsumedLast100']/1000000000,2);
-$ya10 = round($graphData[1]['ethConsumedLast100']/1000000000,2);
-$ya11 = round($graphData[0]['ethConsumedLast100']/1000000000,2);
+//convert to ETH from gwei and divide by 100 to get average per block over last 100 blocks
 
-$yb1 = round($graphData[10]['meanDelayLast100'],2);
-$yb2 = round($graphData[9]['meanDelayLast100'],2);
-$yb3 = round($graphData[8]['meanDelayLast100'],2);
-$yb4 = round($graphData[7]['meanDelayLast100'],2);
-$yb5 = round($graphData[6]['meanDelayLast100'],2);
-$yb6 = round($graphData[5]['meanDelayLast100'],2);
-$yb7 = round($graphData[4]['meanDelayLast100'],2);
-$yb8 = round($graphData[3]['meanDelayLast100'],2);
-$yb9 = round($graphData[2]['meanDelayLast100'],2);
-$yb10 = round($graphData[1]['meanDelayLast100'],2);
-$yb11 = round($graphData[0]['meanDelayLast100'],2);
+$ya1 = round($graphData[19]['ethConsumedLast100']/100000000000,2);
+$ya2 = round($graphData[18]['ethConsumedLast100']/100000000000,2);
+$ya3 = round($graphData[17]['ethConsumedLast100']/100000000000,2);
+$ya4 = round($graphData[16]['ethConsumedLast100']/100000000000,2);
+$ya5 = round($graphData[15]['ethConsumedLast100']/100000000000,2);
+$ya6 = round($graphData[14]['ethConsumedLast100']/100000000000,2);
+$ya7 = round($graphData[13]['ethConsumedLast100']/100000000000,2);
+$ya8 = round($graphData[12]['ethConsumedLast100']/100000000000,2);
+$ya9 = round($graphData[11]['ethConsumedLast100']/100000000000,2);
+$ya10 = round($graphData[10]['ethConsumedLast100']/100000000000,2);
+$ya11 = round($graphData[9]['ethConsumedLast100']/100000000000,2);
+$ya12 = round($graphData[8]['ethConsumedLast100']/100000000000,2);
+$ya13 = round($graphData[7]['ethConsumedLast100']/100000000000,2);
+$ya14 = round($graphData[6]['ethConsumedLast100']/100000000000,2);
+$ya15 = round($graphData[5]['ethConsumedLast100']/100000000000,2);
+$ya16 = round($graphData[4]['ethConsumedLast100']/100000000000,2);
+$ya17 = round($graphData[3]['ethConsumedLast100']/100000000000,2);
+$ya18 = round($graphData[2]['ethConsumedLast100']/100000000000,2);
+$ya19 = round($graphData[1]['ethConsumedLast100']/100000000000,2);
+$ya20 = round($graphData[0]['ethConsumedLast100']/100000000000,2);
+
+
+$yb1 = round($graphData[19]['meanDelayLast100'],2);
+$yb2 = round($graphData[18]['meanDelayLast100'],2);
+$yb3 = round($graphData[17]['meanDelayLast100'],2);
+$yb4 = round($graphData[16]['meanDelayLast100'],2);
+$yb5 = round($graphData[15]['meanDelayLast100'],2);
+$yb6 = round($graphData[14]['meanDelayLast100'],2);
+$yb7 = round($graphData[13]['meanDelayLast100'],2);
+$yb8 = round($graphData[12]['meanDelayLast100'],2);
+$yb9 = round($graphData[11]['meanDelayLast100'],2);
+$yb10 = round($graphData[10]['meanDelayLast100'],2);
+$yb11 = round($graphData[9]['meanDelayLast100'],2);
+$yb12 = round($graphData[8]['meanDelayLast100'],2);
+$yb13 = round($graphData[7]['meanDelayLast100'],2);
+$yb14 = round($graphData[6]['meanDelayLast100'],2);
+$yb15 = round($graphData[5]['meanDelayLast100'],2);
+$yb16 = round($graphData[4]['meanDelayLast100'],2);
+$yb17 = round($graphData[3]['meanDelayLast100'],2);
+$yb18 = round($graphData[2]['meanDelayLast100'],2);
+$yb19 = round($graphData[1]['meanDelayLast100'],2);
+$yb20 = round($graphData[0]['meanDelayLast100'],2);
+
 
 $latestblock = $row['latestblockNum'];
 $ethprice = $row['ETHpriceUSD'];
