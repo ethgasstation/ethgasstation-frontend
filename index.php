d10d9310f89be6bf816b62a4f9fdc8d976d2edc6
jake <jacobelkins@protonmail.com>
2017-03-25 07:27:31 -0400

    fix

--- index.php
+++ index.php
@@ -293,7 +293,7 @@
             <div class="col-md-8 col-sm-12 col-xs-12">
               <div class="x_panel tile fixed_height_420">
                 <div class="x_title">
-                  <h2>Miner Rankings: <small> Support for user transactions</small></h2>
+                  <h4>Miner Rankings: <small> Support for user transactions</small></h4>
                   <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
