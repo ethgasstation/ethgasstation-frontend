72909793ad0eb4ce6aebba85d914f471053a7f6b
jake <jacobelkins@protonmail.com>
2017-03-25 12:45:45 -0400

    url

--- index.php
+++ index.php
@@ -128,10 +128,10 @@
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu">
-                    <li id="usd"><a href="#"> USD<span class="pull-right"><i class="fa fa-check"></i></span></a></li>
-                    <li id="eur"><a href="#"> EUR</a></li>
-                    <li id="gbp"><a href="#"> GBP</a></li>
-                    <li id="cny"><a href="#"> CNY</a></li>
+                    <li id="usd"><a href="#USD"> USD<span class="pull-right"><i class="fa fa-check"></i></span></a></li>
+                    <li id="eur"><a href="#EUR"> EUR</a></li>
+                    <li id="gbp"><a href="#GBP"> GBP</a></li>
+                    <li id="cny"><a href="#CNY"> CNY</a></li>
                   </ul>
                 </li>
               <p class="navbar-text navbar-left" style="padding-left: 5px"><strong><?php echo "Estimates over last 10,000 blocks - Last update: Block <span style = 'color:#1ABB9C'> $latestblock" ?></strong></span>  
