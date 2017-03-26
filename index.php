63ae856415aebaa192fbf8fda808e97b40e01e8d
jake <jacobelkins@protonmail.com>
2017-03-26 07:22:42 -0400

    graph smaller

--- index.php
+++ index.php
@@ -178,7 +178,7 @@
           <div class="row">
 
           <!-- Network Activity Graph -->
-             <div class="col-md-8 col-sm-12 col-xs-12">
+             <div class="col-md-8 col-sm-8 col-xs-12">
                  <div class="x_panel tile fixed_height_320">
                      <div class="x_title">
                         <h4>Recent Network Activity: <small>Gas Demand / Average Wait Time (100 block intervals)</small></h4>
@@ -213,7 +213,7 @@
 
         <div class="row">
 
-            <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-4 col-xs-12">
               <div class="x_panel tile fixed_height_320">
                 <div class="x_title">
                   <h4>Transaction Count by Gas Price</h4>
