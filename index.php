81c15a0bfa35674148ff996081a23624ad9b554c
jake <jacobelkins@protonmail.com>
2017-03-26 08:13:31 -0400

    graph smaller

--- index.php
+++ index.php
@@ -219,7 +219,7 @@
                   <h4>Transaction Count by Gas Price</h4>
                   <div class="clearfix"></div>
                 </div>
-                <div class="x_content">
+                <div class="x_content" id="txCount">
                   <canvas id="mybarChart2" height="210" width="300"></canvas>
                 </div>
               </div>
