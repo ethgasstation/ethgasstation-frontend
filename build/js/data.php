147e182f5b4398e856590a8d77416f021c19975c
jake <jacobelkins@protonmail.com>
2017-04-01 16:24:48 -0400

    safeLow

--- build/js/data.php
+++ build/js/data.php
@@ -364,7 +364,7 @@ else
 
 foreach ($miners as $key => $val)
 {
-	echo ("$val['emptyAdjustedRate']\n");
+	echo ($val['emptyAdjustedRate']."\n");
 }
 
 $cat1HashPower = $cat2HashPower = $cat3HashPower = $cat4HashPower = $cat5HashPower = 0;
