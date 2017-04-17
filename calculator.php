f7ec00b2be997e703f4111a64e4c690477d9e738
jake <jacobelkins@protonmail.com>
2017-04-17 01:18:37 -0400

    train

---
+++ calculator.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ETH Gas Station | Consumer oriented metrics for the Ethereum gas market </title>
+
+    <meta name="keywords" content="Ethereum ETH gas blockchain market price transactions miners users">
+    <meta name="description" content="User oriented, real-time metrics on gas price, wait times, and miner policies on the Ethereum netowrk">
+    
+    <meta property="og:title" content="ETH Gas Station" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="images/ETHGasStation.png" />
+    <meta property="og:url" content="http://ethgasstation.info" />
+
+
+    <!-- Bootstrap -->
+    <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="images/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="images/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- Custom Theme Style -->
+    <link href="build/css/custom.css" rel="stylesheet">
+     <?php include 'datacalc.php'; ?>
+
+   
+
+
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+
+<!-- Sidebar -->
+
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.php" class="site_title"><img src="images/ETHgas.png" style="height:32px;width:32px"> <span>ETH Gas Station</span></a>
+            </div>
+            
+            <div class="clearfix"></div>
+            <br />
+
+          <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a href="index.php"><i class ="fa fa-home"></i>Main Page</a></li>
+                  <li><a href="about.html"><i class="fa fa-question"></i> What Is This Site?</a></li>
+                  <li><a><i class="fa fa-bookmark-o"></i>FAQ<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="gasrecs.html">Gas Price Recommendations</a></li>
+                      <li><a href="FAQpage.html">Other FAQ</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-link"></i> External Links <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="https://coincenter.org/entry/what-is-ethereum" target="_blank">What is Ethereum?</a></li>
+                      <li><a href="https://www.ethereum.org/ether" target="_blank">Ethereum FAQ</a></li>
+                      <li><a href="https://media.consensys.net/ethereum-gas-fuel-and-fees-3333e17fe1dc#.krqnhnkav" target="_blank">What is gas?</a></li>
+                      <li><a href="https://blog.ethereum.org/2016/10/31/uncle-rate-transaction-fee-analysis/" target="_blank">Gas Market: Advanced reading</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- /sidebar -->
+        </div>
+      </div>
+
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+
+              <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                  <a href="#" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-globe"></i><span style="color:#768399"> Change Currency</span>
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li id="usd"><a href="#"> USD<?php if($currency=='usd'){echo'<span class="pull-right"><i class="fa fa-check"></i></span>';}?></a></li>
+                    <li id="eur"><a href="#"> EUR<?php if($currency=='eur'){echo'<span class="pull-right"><i class="fa fa-check"></i></span>';}?></a></li>
+                    <li id="gbp"><a href="#"> GBP<?php if($currency=='gbp'){echo'<span class="pull-right"><i class="fa fa-check"></i></span>';}?></a></li>
+                    <li id="cny"><a href="#"> CNY<?php if($currency=='cny'){echo'<span class="pull-right"><i class="fa fa-check"></i></span>';}?></a></li>
+                  </ul>
+                </li>
+            </ul>
+            </nav>
+          </div>
+         </div>
+
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+
+        
+          <div class="row">
+              <div class="col-md-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h4>Transaction Inputs</h4>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left input_mask">
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Gas Used<span class="required">*</span></label>
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input type="number" class="form-control" placeholder="21000" id="gas_used">
+                        </div>
+                      </div>
+                      
+                      <div class="form-group">
+                        <label class="col-md-3 col-sm-3 col-xs-12 control-label">Gas Price<span class="required">*</span></label>
+
+                      <div class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="checkbox">
+                          <label>
+                          <input type="checkbox" class="flat" id="fast"> Fastest
+                          <?php echo ("(".$gasPriceRecs['Fastest']." Gwei)") ?></label>
+                        </div>
+                      <div class="checkbox">
+                        <label>
+                        <input type="checkbox" class="flat" checked="checked" id="avg"> Average
+                        <?php echo ("(".$gasPriceRecs['Average']." Gwei)") ?></label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                        <input type="checkbox" class="flat" id="cheap"> Cheapest
+                        <?php echo ("(".$gasPriceRecs['Cheapest']." Gwei)") ?></label>
+                    </div>
+                     <div class="checkbox">
+                        <label>
+                        <input type="checkbox" class="flat" id="other"> Other
+                        </label>
+                    </div>
+                    <div>
+                      <input type="number" class="form-control" placeholder="(Gwei)" id="oth_val">
+                    </div>
+                  </div>
+                </div>
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+						              <button class="btn btn-primary" type="reset" id="reset">Reset</button>
+                          <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                      </div>
+
+                    </form>
+                  </div>
+                </div>
+            </div>
+
+                 <div class="col-md-6 col-xs-12">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h4 id="txArgs">Predictions:</h4>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Outcome</th>
+                          <th></th>
+                
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Mean Time to Confirm (Blocks)</td>
+                          <td id="meanBlocks"></td>
+                        </tr>
+                        <tr>
+                          <td>Mean Time to Confirm (Seconds)</td>
+                          <td id="meanSecs"></td>
+                        </tr>
+                         <tr>
+                          <td>Transaction fee (ETH)</td>
+                          <td id="txEth"></td>
+                        </tr>
+                         <tr>
+                          <td>Transaction fee (Fiat)</td>
+                          <td id="txFiat"></td>
+                        </tr>
+                         
+                      </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+      </div>
+
+    <!-- /misc transactions -->
+
+  </div>
+</div>
+             
+<!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+
+      </div>
+    </div>
+
+ <!-- jQuery -->
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+ <!-- Bootstrap -->
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+ <!-- Chart.js -->
+    <script src="vendors/Chart.js/dist/Chart.min.js"></script>
+    
+
+<!-- Custom Theme Scripts -->
+   <script>
+
+
+      //Curency Support
+      
+            $("#eur").click(function(){     
+                location = "http://ethgasstation.info/calculator.php?curr=eur";                              
+            });
+            
+            $("#usd").click(function(){
+                location = "http://ethgasstation.info/calculator.php?curr=usd";
+            });
+          
+            $("#cny").click(function(){
+                location = "http://ethgasstation.info/calculator.php?curr=cny";                                      
+            });
+
+            $("#gbp").click(function(){
+                location = "http://ethgasstation.info/calculator.php?curr=gbp";                       
+            });
+
+            $('input.flat').change(function(){
+                $('input.flat').not(this).prop('checked',false);
+            })
+            $('#oth_val').change(function(){
+               $('input.flat').prop('checked',false);
+                $('#other').prop('checked',true);
+            })
+            $('#reset').click(function(){
+              $('#meanBlocks').html("");
+              $('#meanSecs').html("");
+              $('#txEth').html("");
+              $('#txFiat').html("");
+              $('#txArgs').html("Predictions:");
+              $("#oth_val").parent().next(".validation").remove();
+              $("#gas_used").parent().next(".validation").remove();
+            })
+
+            $('form').submit(function(event){
+              $currency = <?php echo ($currency) ?>;
+              $exchangeRate =<?php echo ($exchangeRate) ?>;
+              $cat1 = <?php echo ($calcParams['cat1']) ?>;
+              $cat3 = <?php echo ($calcParams['cat3']) ?>;
+              $cat4 = <?php echo ($calcParams['cat4']) ?>;
+              $cons = <?php echo ($calcParams['const']) ?>;
+              $transfer = <?php echo ($calcParams['transferCat']) ?>;
+              $gasUsedParam = <?php echo ($calcParams['gasUsedCont']) ?>;
+              if(!$('#gas_used').val()){
+                $("#gas_used").parent().next(".validation").remove();
+                $txGasUsed = 21000;
+              }
+              else if ($('#gas_used').val() > 4000000){
+                $string = "<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter gas used less than 4,000,000 (block limit)";
+                  $("#gas_used").parent().after($string);
+                    event.preventDefault(); // prevent form from POST to server
+                    $('#gas_used').focus();
+                    focusSet = true;
+                    return;
+              }
+              else{
+                $("#gas_used").parent().next(".validation").remove();
+                $txGasUsed = $('#gas_used').val();
+              }
+              if ($txGasUsed != 21000){
+                $transfer = 0
+              }
+              if($('#other').prop('checked')===true){
+                $otherGasPrice = $('#oth_val').val()
+                if (!$otherGasPrice){
+                    if ($("#oth_val").parent().next(".validation").length == 0){ // only add if not added
+                      $("#oth_val").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter gas price in gwei</div>");
+                    }
+                    event.preventDefault(); // prevent form from POST to server
+                    $('#oth_val').focus();
+                    focusSet = true;
+                    return;
+                } 
+                else {
+                $("#oth_val").parent().next(".validation").remove();//remove it
+                $txGasPrice = $("#oth_val").val()
+                if ($txGasPrice >= <?php echo($gasPriceRecs['Fastest']) ?>){
+                  $blocksWait = Math.exp($cons + $cat4 + ($gasUsedParam * $txGasUsed/100000)+ $transfer);
+                  $blocksWait = Number(($blocksWait).toFixed(2))
+                }
+                else if ($txGasPrice > <?php echo($gasPriceRecs['Average']) ?> && $txGasPrice < <?php echo($gasPriceRecs['Fastest']) ?>){
+                  $blocksWait = Math.exp($cons + $cat3 + ($gasUsedParam * $txGasUsed/100000)+ $transfer);
+                  $blocksWait = Number(($blocksWait).toFixed(2))
+                }
+                else if ($txGasPrice === <?php echo($gasPriceRecs['Average']) ?>) {
+                  $blocksWait = Math.exp($cons + ($gasUsedParam * $txGasUsed/100000)+ $transfer);
+                  $blocksWait = Number(($blocksWait).toFixed(2))
+                }
+                else if ($txGasPrice >= <?php echo($gasPriceRecs['Cheapest']) ?> && $txGasPrice < <?php echo($gasPriceRecs['Average']) ?>){
+                  $blocksWait = Math.exp($cons + $cat1 + ($gasUsedParam * $txGasUsed/100000) + $transfer);
+                  $blocksWait = Number(($blocksWait).toFixed(2))
+                }
+                else {
+                  $string = "<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter gas price >= <?php echo($gasPriceRecs['Cheapest'])?> gwei</div>";
+                  $("#oth_val").parent().after($string);
+                    event.preventDefault(); // prevent form from POST to server
+                    $('#oth_val').focus();
+                    focusSet = true;
+                    return;
+                }
+              }
+            }
+
+              else {
+                if ($('#fast').prop('checked')===true){
+                  $txGasPrice = <?php echo($gasPriceRecs['Fastest']) ?>;
+                  $blocksWait = Math.exp($cons + $cat4 + ($gasUsedParam * $txGasUsed/100000)+$transfer);
+                  $blocksWait = Number(($blocksWait).toFixed(2))
+                }
+                else if ($('#avg').prop('checked')===true){
+                  $txGasPrice = <?php echo($gasPriceRecs['Average']) ?>;
+                  $blocksWait = Math.exp($cons + ($gasUsedParam * $txGasUsed/100000)+$transfer);
+                  $blocksWait = Number(($blocksWait).toFixed(2))
+                }
+                else if ($('#cheap').prop('checked')===true){
+                  $txGasPrice = <?php echo($gasPriceRecs['Cheapest']) ?>;
+                  $blocksWait = Math.exp($cons + $cat1 + ($gasUsedParam * $txGasUsed/100000)+$transfer);
+                  $blocksWait = Number(($blocksWait).toFixed(2))
+                }
+               $('#oth_val').val("");
+               $("#oth_val").parent().next(".validation").remove();
+              }
+
+              
+              event.preventDefault();
+              $txArgs = "Predictions: <small><span style='color:red'> Gas Used = "+ $txGasUsed + "; Gas Price = " + $txGasPrice + " gwei</span></small>";
+              $('#txArgs').html($txArgs);
+              
+              $txMeanSecs = Math.round($blocksWait*14.5);
+
+              $txFeeEth = $txGasPrice/1e9 * $txGasUsed;
+              $txFeeEth = Number(($txFeeEth).toFixed(6))
+              $txFeeFiat = $txFeeEth * $exchangeRate;
+              $txFeeFiat = Number(($txFeeFiat).toFixed(3));
+              
+              
+
+              $('#meanBlocks').html($blocksWait);
+              $('#meanSecs').html($txMeanSecs);
+              $('#txEth').html($txFeeEth);
+              if ($currency='usd'){
+                $string="$"+$txFeeFiat
+              $('#txFiat').html($string);
+              }
+              
+
+          })
+
+
+
+ </script>
+
+
+
+    <script src="build/js/custom3.js"></script>
+    
+	
+  </body>
+</html>
---
+++ datacalc.php
@@ -0,0 +1,67 @@
+<?php
+//setting header to json
+//header('Content-Type: application/json');
+
+//database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'ethgas');
+define('DB_PASSWORD', 'station');
+define('DB_NAME', 'tx');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+	die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+
+$query = "SELECT * FROM txDataLast10k ORDER BY id DESC LIMIT 1";
+$result = $mysqli->query($query);
+$row = $result->fetch_assoc();
+
+
+// Get values for Misc transactions table
+
+$latestblock = $row['latestblockNum'];
+$ethprice = $row['ETHpriceUSD'];
+$ethpriceEUR = $row['ETHpriceEUR'];
+$ethpriceCNY = $row['ETHpriceCNY'];
+$ethpriceGBP = $row['ETHpriceGBP'];
+
+if(isset($_GET['curr']) && !empty($_GET['curr'])){
+	$currency = $_GET['curr'];
+} 
+else {$currency = 'usd';}
+
+if ($currency == 'usd'){
+    $exchangeRate = $ethprice;
+}
+elseif ($currency == 'eur'){
+    $exchangeRate = $ethpriceEUR;
+}
+elseif ($currency == 'cny'){
+    $exchangeRate = $ethpriceCNY;
+}
+elseif ($currency == 'gbp'){
+    $exchangeRate = $ethpriceGBP;
+}
+
+$calcParamString = file_get_contents("calc.html");
+$calcParams = json_decode($calcParamString, true);
+
+$gasPriceRecString = file_get_contents("ethgasAPI.html");
+$gasPriceRecs = json_decode($gasPriceRecString, true);
+
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+
+
+
+?>
