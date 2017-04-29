03bb7a5b156b938f5eb592f0151d9a03e08435bb
jake <jacobelkins@protonmail.com>
2017-04-28 21:02:26 -0400

    e

--- backend/gasStationMined2.js
+++ backend/gasStationMined2.js
@@ -24,7 +24,7 @@ var txCounter=0;
 var filter = web3.eth.filter('latest');
 var filter2 = web3.eth.filter('pending');
 
-function postObject(){};
+
 
 
 filter.watch(function(err,blockHash)
@@ -32,7 +32,6 @@ filter.watch(function(err,blockHash)
     if (err){
         console.error(err);
     }
-    var post = new postObject();
     web3.eth.getBlock(blockHash, function (err, block)
     {
         if (err){
@@ -41,11 +40,8 @@ filter.watch(function(err,blockHash)
         }
         if (block != null)
         {
-            post['tsMined'] = Math.round(+new Date()/1000);
-            post['miner'] = block.miner;
-            post['blockGasUsed'] = block.gasUsed;
-            post['blockGasLimit'] = block.gasLimit;
-            post['minedBlock'] = block.number;
+            var ts = Math.round(+new Date()/1000);
+        
 
             if (block.transactions.length === 0)
             {
@@ -56,7 +52,7 @@ filter.watch(function(err,blockHash)
             }
             else
             {   
-                post['emptyBlock'] = false;
+    
                 for(x=0; x <block.transactions.length; x++)
                 {
                     console.log("loop " + x)
@@ -73,23 +69,30 @@ filter.watch(function(err,blockHash)
                             var gasPrice = tx.gasPrice.toString(10);
                             gasPrice = gasPrice/1e9; //convert to Gwei
                             var gasPriceCat = getGasPriceCat(gasPrice);
-                            post['minedGasPrice'] = gasPrice;
-                            post['minedGasPriceCat'] = gasPriceCat;
-                            post['fromAddress'] = tx.from;
-                            post['toAddress'] = tx.to;
-                            post['txHash'] = tx.hash;
+                            
 
                             web3.eth.getTransactionReceipt(tx.hash, function (err, receipt)
                             {
-                                console.log("loop2 " + receipt.transactionHash)
                                 if (err)
                                 {
                                     console.error(err.stack);
                                 }
                                 if (receipt != null){  
-                                    console.log("loop3 " + receipt.transactionHash)
-                                    post['gasused'] = receipt.gasUsed;
-                                    console.log(post);
+                                    var post = {
+                                        txHash: receipt.transactionHash,
+                                        minedBlock: receipt.blockNumber,
+                                        toAddress:receipt.to,
+                                        fromAddress:receipt.from,
+                                        gasused: receipt.gasUsed,
+                                        miner: block.miner,
+                                        blockGasUsed: block.gasUsed,
+                                        blockGasLimit: block.gasLimit,
+                                        minedGasPrice:gasPrice,
+                                        minedGasPriceCat:gasPriceCat,
+                                        tsMined: ts,
+                                        emptyBlock:false
+                                    }
+                            
                                     writeData(post, 'minedtransactions');
                                 }
                                 
@@ -126,13 +129,11 @@ filter2.watch(function(err, txHash)
         console.error(err.stack);
         return;
     }
-    
-    var post2 = new postObject();
 
     if (txHash != null)
     {
-        post2['txHash'] = txHash;
-        post2['tsPosted'] = Math.round(+new Date()/1000);
+        
+        var ts2 = Math.round(+new Date()/1000);
         
         web3.eth.getBlockNumber(function (err,blockNum)
         {
@@ -140,7 +141,7 @@ filter2.watch(function(err, txHash)
             {
                 console.log(err.stack);
             }
-            post2['postedBlock'] = blockNum;
+            
         })
 
         web3.eth.getTransaction(txHash, function(err,result)
@@ -154,9 +155,16 @@ filter2.watch(function(err, txHash)
             if (result != null)
             {    
                     var gasPrice = result.gasPrice.toString(10);
-                    post2['gasPrice'] = gasPrice/1e9;
-                    post2['gasPriceCat'] = getGasPriceCat(post2['gasPrice']);
-                    post2['gasOffered'] = result.gas;
+                    gasPrice = gasPrice/1e9;
+                    var gasPriceCat = getGasPriceCat(gasPrice);
+                    var post2 = {
+                        txHash: result.txHash,
+                        postedBlock: blockNum,
+                        gasOffered: result.gas,
+                        gasPrice: gasPrice,
+                        gasPriceCat: gasPriceCat,
+                        tsPosted: ts2
+                    }
                     writeData(post2, 'transactions');
             }
         
