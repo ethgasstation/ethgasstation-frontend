7c4fe315eaed021883cc44eb6107be4777ef1378
jake <jacobelkins@protonmail.com>
2017-05-07 17:37:50 -0400

    e

--- minerTable.php
+++ minerTable.php
@@ -118,10 +118,11 @@
         <!-- /top navigation -->
 
         <!-- page content -->
+        
         <div class="right_col" role="main">
           <div class="row">
               <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
+                <div class="x_panel tile fixed_height_420">
                   <div class="x_title">
                     <h4>Miner Gas Policies and Block Stats</h4>
                     <div class="clearfix"></div>
@@ -169,9 +170,8 @@
                       ?>
                         </tbody>
                     </table>
-
                  </div>
-    
+                 
     
         </div>
     </div>
