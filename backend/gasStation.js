c954603ccdd5f73f1f81fa1208e15db6bf588682
jake <jacobelkins@protonmail.com>
2017-05-07 20:20:21 -0400

    e

--- backend/gasStation.js
+++ backend/gasStation.js
@@ -22,11 +22,6 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
-fs.readFile('file', 'utf8', function (err, data) {
-  if (err) throw err;
-  obj = JSON.parse(data);
-});
-
 validationStatus = {};
 watchedTx = [];
 
