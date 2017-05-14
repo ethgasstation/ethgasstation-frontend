ca09b1817f50e7f97dd8913f3e29775bd4b73545
jake <jacobelkins@protonmail.com>
2017-05-14 19:13:25 -0400

    e

--- backend/miner.py
+++ backend/miner.py
@@ -70,7 +70,7 @@ print (minerBlocks)
 
 minerData['const'] = 1
 print(minerData)
-#minerData = minerData.loc[:,['gasUsed','main']].dropna()
+print(minerData.loc[minerData['gasUsed'].isnull())
 print(minerData)
 model = sm.OLS(minerData['gasUsed'], minerData['const'])
 results = model.fit()
