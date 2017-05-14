82bd983d0c5c7b5d2cb53d267ce90d6dfb8a3953
jake <jacobelkins@protonmail.com>
2017-05-14 10:22:23 -0400

    e

--- backend/miner.py
+++ backend/miner.py
@@ -3,13 +3,16 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 
+startBlock = sys.argv[1]
+endBlock = sys.argv[2]
+
 cnx = mysql.connector.connect(user='ethgas', password='station', host='127.0.0.1', database='tx')
 cursor = cnx.cursor()
 
 # First Query to Determine Block TIme, and Estimate Miner Policies
-query = ("SELECT * FROM speedo where speedo.blockNum>=3705683")
+query = ("SELECT * FROM speedo where blockNum>= %s and blockNum < %s")
 
-cursor.execute(query)
+cursor.execute(query, (startBlock, endBlock))
 head = cursor.column_names
 
 minerData = pd.DataFrame(cursor.fetchall())
@@ -26,7 +29,7 @@ minerBlocks = mainBlocks.groupby('miner').sum()
 
 print (minerBlocks)
 
-minerBlocks = minerBlocks.drop(['id', 'blockNum', 'gasLimit', 'includedBlockNum', 'speed', 'avgWait'], axis=1)
+minerBlocks = minerBlocks.drop(['id', 'blockNum', 'gasLimit', 'includedBlockNum', 'uncle'], axis=1)
 
 # generates following aggregates by miner:  total mainblocks, total gasUsed in mainBlocks, total uncleInlcusions, 
 
@@ -37,7 +40,7 @@ print (minerBlocks)
 uncleBlocks = minerData.loc[minerData['uncle']==1]
 uncleBlocks['awards']= 5*((uncleBlocks['includedBlockNum'] - uncleBlocks['blockNum'])/8) 
 minerUncleBlocks = uncleBlocks.groupby('miner').sum()
-minerUncleBlocks = minerUncleBlocks.drop(['id', 'blockNum', 'gasLimit', 'speed', 'averageTx', 'numUncs', 'numTx'], axis=1)
+minerUncleBlocks = minerUncleBlocks.drop(['id', 'blockNum', 'gasLimit', 'averageTx', 'numUncs', 'numTx', 'main'], axis=1)
 
 print (minerUncleBlocks)
 
