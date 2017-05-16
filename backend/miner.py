0c57abe4222e57cd8aef6055c5915167ae7fa36e
jake <jacobelkins@protonmail.com>
2017-05-15 20:04:04 -0400

    e

--- backend/miner.py
+++ backend/miner.py
@@ -127,7 +127,7 @@ results = model.fit()
 print (results.summary())
 
 miner2Data = minerData.loc[minerData['miner'] == '0x61c808d82a3ac53231750dadc13c777b59310bd9', :]
-model = sm.OLS(miner1Data['uncle'], miner1Data[['const','gasUsedPerM']])
+model = sm.OLS(miner2Data['uncle'], miner2Data[['const','gasUsedPerM']])
 results = model.fit()
 print (results.summary())
 
