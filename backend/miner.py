c8bc9dfef35fb36b79faee38f58cd4f255a83dd6
jake <jacobelkins@protonmail.com>
2017-05-16 20:32:44 -0400

    3

--- backend/miner.py
+++ backend/miner.py
@@ -110,6 +110,8 @@ minerBlocks['totReward'] = minerBlocks['fee'] + minerBlocks['totRewardminusTxFee
 minerBlocks['totalBlocks'] = minerBlocks['main'] + minerBlocks['uncle']
 minerBlocks['avgReward'] = minerBlocks['totReward'] / minerBlocks['totalBlocks']
 minerBlocks['uncRatio'] = minerBlocks['uncle'] / minerBlocks['totalBlocks']
+minerBlocks['avgUncleAward'] = minerBlocks['uncleAwards'] / minerBlocks['uncle']
+minerBlocks['mainAwardwoFee'] =  minerBlocks['totRewardminusTxFees'] / minerBlocks['totalBlocks']
 minerBlocks = minerBlocks.sort_values('avgReward')
 
 print(minerBlocks)
@@ -120,6 +122,9 @@ minerData['gasUsedPerM'] = minerData['gasUsed']/1e6
 model = sm.OLS(minerData['uncle'], minerData[['const','gasUsedPerM']])
 results = model.fit()
 print (results.summary())
+
+dictResults = dict(results.params)
+print (dictResults)
 
 miner1Data = minerData.loc[minerData['miner'] == '0xea674fdde714fd979de3edf0f56aa9716b898ec8', :]
 model = sm.OLS(miner1Data['uncle'], miner1Data[['const','gasUsedPerM']])
