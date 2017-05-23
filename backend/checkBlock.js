987c88ea527fe5c4ea2ccf1ac7ee9e0c4b5ece04
jake <jacobelkins@protonmail.com>
2017-05-22 21:52:11 -0400

    e

--- backend/checkBlock.js
+++ backend/checkBlock.js
@@ -4,6 +4,7 @@ var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var hash = process.argv[2];
 
 var receipt = web3.eth.getBlock(hash);
+delete receipt.logsBloom
 var json = JSON.stringify(receipt);
 console.log(json);
 /*
