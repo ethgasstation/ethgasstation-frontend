f16ff19999c058cb19cd84ea895f7806146646fd
jake <jacobelkins@protonmail.com>
2017-05-26 22:32:46 -0400

    s

--- build/php/profits.php
+++ build/php/profits.php
@@ -50,6 +50,7 @@ elseif ($currency == 'gbp'){
 try{
     $profitRaw = file_get_contents("http://localhost/json/profit.json");
     $profitTable = json_decode($profitRaw, true);
+    echo($profitTable)
 
 } catch (Exception $e) {
     echo 'waith for tables to be populated';
