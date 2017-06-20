b2c39a92c2c50c906257e846e024939847d80720
jake <jacobelkins@protonmail.com>
2017-06-20 15:19:56 -0400

    const

--- backend/gascalc2.py
+++ backend/gascalc2.py
@@ -449,7 +449,8 @@ dep['gasCat4'] = (txData2['gasused']> quantiles[.9]).astype(int)
 
 dep['cons'] = 1
 
-txData2['logDelay'] = txData2['delay'].apply(np.log)
+#txData2['logDelay'] = txData2['delay'].apply(np.log)
+txData2['logDelay'] = txData2['delay']
 txData2 = txData2.dropna()
 indep = txData2['logDelay']
 
