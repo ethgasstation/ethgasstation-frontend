9afc2d1d47a8ee6c6fe9ebcb2e38497931d60c47
jake <jacobelkins@protonmail.com>
2017-07-03 14:40:42 -0400

    model

--- backend/gascalc2.py
+++ backend/gascalc2.py
@@ -429,10 +429,10 @@ priceWait = txData2.loc[:, ['minedGasPrice', 'delay']]
 priceWait.loc[priceWait['minedGasPrice']>=40, 'minedGasPrice'] = 40
 priceWait = priceWait.loc[(priceWait['minedGasPrice']<=10) | (priceWait['minedGasPrice']==20) | (priceWait['minedGasPrice'] == 40), ['minedGasPrice', 'delay']]
 priceWait.loc[priceWait['minedGasPrice']<1, 'minedGasPrice'] = 0
-priceWait['delay'] = priceWait['delay'].apply(np.log)
+#priceWait['delay'] = priceWait['delay'].apply(np.log)
 priceWait = priceWait.groupby('minedGasPrice').mean()
 priceWait.reset_index(inplace=True)
-priceWait['delay'] = priceWait['delay'].apply(np.exp)
+#priceWait['delay'] = priceWait['delay'].apply(np.exp)
 priceWait['delay'] = priceWait['delay']*blockInterval/float(60)
 
 
