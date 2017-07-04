7d95e1da205f93a2d38470a6c792718e9c8f4dc8
root <jacobelkins@protonmail.com>
2017-07-04 19:07:35 -0400

    prediction Model Final

--- model.py
+++ model.py
@@ -33,7 +33,7 @@ print('zero/neg confirm times: ')
 print(predictData[predictData['confirmTime']<=0].count())
 
 predictData[predictData['confirmTime'] <= 0] = np.nan
-predictData[predictData['confirmTime'] >= 500] = np.nan
+predictData.loc[predictData['confirmTime'] >= 500, 'confirmTime'] = 500
 predictData = predictData.dropna(how='any')
 
 print ('cleaned transactions: ')
@@ -45,9 +45,26 @@ print(predictData['confirmTime'].count())
 predictData['logCTime'] = predictData['confirmTime'].apply(np.log)
 
 predictData['transfer'] = predictData['gasOffered'].apply(lambda x: 1 if x ==21000 else 0) 
-predictData['gasOffered'] = predictData['gasOffered'].apply(lambda x: x/4710000)
+predictData['pctGasOffered'] = predictData['gasOffered'].apply(lambda x: x/4710000)
+
+transactionGas = float(21000)/4710000
+
+quantiles= predictData['pctGasOffered'].quantile([.5, .75, .9, 1])
+
+#dep['gasCat1'] = (txData2['gasused'] == 21000).astype(int)
+predictData['gasCat2'] = ((predictData['pctGasOffered']>transactionGas) & (predictData['pctGasOffered']<=quantiles[.75])).astype(int)
+predictData['gasCat3'] = ((predictData['pctGasOffered']>quantiles[.75]) & (predictData['pctGasOffered']<=quantiles[.9])).astype(int)
+predictData['gasCat4'] = (predictData['pctGasOffered']> quantiles[.9]).astype(int)
+
+print('median pctGasOffered')
+print(transactionGas)
+print(quantiles[.5])
+
+print('confirmTImes')
 print(predictData['confirmTime'].min())
 print(predictData['confirmTime'].max())
+
+print('hashPowerAccepting')
 print(predictData['hashPowerAccepting'].min())
 print(predictData['hashPowerAccepting'].max())
 
@@ -65,7 +82,7 @@ print(predictData['txAt'].mean())
 
 
 
-y, X = dmatrices('confirmTime ~ transfer + hashPowerAccepting + pctLimitGasAbove + pctLimitGasAt + gasOffered + txAbove + txAt', data = predictData, return_type = 'dataframe')
+y, X = dmatrices('confirmTime ~ hashPowerAccepting + pctLimitGasAbove + pctLimitGasAt + gasCat2 + gasCat3 + gasCat4 + txAbove + txAt', data = predictData, return_type = 'dataframe')
 
 print(y[:5])
 print(X[:5])
@@ -83,7 +100,13 @@ y['txAbove'] = predictData['txAbove']
 y['txAt'] = predictData['txAt']
 y['gasOffered'] = predictData['gasOffered']
 y['transfer'] = predictData['transfer']
+y['gasCat2'] = predictData['gasCat2']
+y['gasCat3'] = predictData['gasCat3']
+y['gasCat4'] = predictData['gasCat4']
 
+print(y)
+
+'''
 y1, X1 = dmatrices('logCTime ~ transfer + hashPowerAccepting + pctLimitGasAbove + pctLimitGasAt + gasOffered + txAbove + txAt', data = predictData, return_type = 'dataframe')
 
 print(y[:5])
@@ -96,33 +119,42 @@ y1['predict'] = results.predict()
 y1['confirmTime'] = predictData['confirmTime']
 y1['predictTime'] = y1['predict'].apply(lambda x: np.exp(x))
 
-with pd.option_context('display.max_rows', 5000, 'display.max_columns', None):
+
+y2, X2 = dmatrices('logCTime ~ hashPowerAccepting + pctLimitGasAbove + pctLimitGasAt + gasCat2 + gasCat3 + gasCat4 + txAbove + txAt', data = predictData, return_type = 'dataframe')
+
+print(y[:5])
+print(X[:5])
+
+model = sm.OLS(y2, X2)
+results = model.fit()
+print (results.summary())
+
+y3, X3 = dmatrices('logCTime ~ hashPowerAccepting + pctLimitGasAbove + pctLimitGasAt + gasCat2 + gasCat3 + gasCat4 + totalTxTxP', data = predictData, return_type = 'dataframe')
+
+print(y[:5])
+print(X[:5])
+
+model = sm.OLS(y3, X3)
+results = model.fit()
+print (results.summary())
+
+y4, X4 = dmatrices('logCTime ~ hashPowerAccepting + gasOffered', data = predictData, return_type = 'dataframe')
+
+print(y[:5])
+print(X[:5])
+
+model = sm.OLS(y4, X4)
+results = model.fit()
+print (results.summary())
+
+
+
+
+
+with pd.option_context('display.max_rows', 500, 'display.max_columns', None):
 
     print(y.loc[(y['gasPrice']==1000) & (y['transfer']==1),:])
-    #print(y1)
+    print(y.loc[(y['gasPrice']>=50000) & (y['transfer']==1),:])
  
-
 '''
-dep = pd.DataFrame()
-indep = pd.DataFrame()
-dep = predictData['confirmTime']
-indep['const'] = predictData['const']
 
-model = sm.Poisson(dep, indep)
-results = model.fit()
-print (results.summary())
-
-indep['hashPowerAccepting'] = predictData['hashPowerAccepting']
-model = sm.Poisson(dep, indep)
-results = model.fit()
-print (results.summary())
-
-model = sm.Poisson(predictData['confirmTime'], [predictData['const'], predictData['hashPowerAccepting'], predictData['gasOffered']])
-results = model.fit()
-print (results.summary())
-
-model = sm.Poisson(predictData['confirmTime'], [predictData['const'], predictData['hashPowerAccepting'], predictData['gasOffered'], predictData['pctLimitGasAbove']])
-results = model.fit()
-print (results.summary())
-
-'''
