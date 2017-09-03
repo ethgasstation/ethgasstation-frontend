0be1321e3f7918e689d7cc2743e6bd91811818a8
jake <jacobelkins@protonmail.com>
2017-09-03 10:20:36 -0400

    test

--- index2.php
+++ index2.php
@@ -122,13 +122,12 @@
               <span class="count_top"><i class="fa fa-tachometer"></i> Current Network Demand</span>
               <?php 
               if ($gpRecs2['netDemand'] == 1){
-                echo("<div class='count red'><p><Normal></p></div>");}
+                echo("<div class='count green'>Normal</div>");}
               elseif (gpRecs2['netDemand'] ==2){
                 echo ("<div class='count red'><High></div>");}
               elseif (gpRecs2['netDemand'] ==3){
                   echo ("<div class='count red'><Very High></div>");}
               ?>
-            </div>
           </div>
           <!-- /top tiles -->
 
