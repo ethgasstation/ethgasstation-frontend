7acc77702f695fe19925d2e03248945459b62e0a
jake <jacobelkins@protonmail.com>
2017-09-02 22:23:19 -0400

    e

--- backend/model6.py
+++ backend/model6.py
@@ -123,7 +123,7 @@ print(y)
 
 print (y.loc[(y['dump']==0) & (y['gasPrice'] < 1000), ['confirmTime', 'predict', 'gasPrice']])
 
-a, B = dmatrices('confirmTime ~ gp1+ gp2+ gp3 + gp4 + dump + ico + txAtAbove + transfer', data = pdValidate, return_type = 'dataframe')
+a, B = dmatrices('confirmTime ~ gp1+ gp2+ gp3 + gp4 + dump + ico + txAtAbove + totalTxFee', data = pdValidate, return_type = 'dataframe')
 
 
 model = sm.GLM(a, B, family=sm.families.Poisson())
