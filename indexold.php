ac09ea3e28eae2d934b52b9bb362a8003e753b2b
jake <jacobelkins@protonmail.com>
2017-09-04 12:37:56 -0400

    slider

--- indexold.php
+++ indexold.php
@@ -149,7 +149,7 @@
                       <form>
                         <div>
                           <label>Gas Used<span class="required">*</span></label>
-                          <input type="number" placeholder="21000" id="gas_used" style="width:35px">
+                          <input type="number" placeholder="21000" id="gas_used" style="width:80px">
                           <label>Avg Time To Confirmation</label>
                           <input type="number" placeholder="5" id="timeToConfirm" style="width:35px">    
                         </div>
