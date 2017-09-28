a3243a4a54569990661c0654a4b32522b1affd92
jake <jsbe70@protonmail.com>
2017-09-27 21:54:56 -0400

    p

--- gasStation.py
+++ gasStation.py
@@ -44,8 +44,8 @@ class Tx ():
 def new_tx_callback(tx_hash):
     try:
         tx_obj = web3.eth.getTransaction(tx_hash)
-        clean_tx = Tx(tx_obj)
-        pending.append(clean_tx)
+        clean_tx = pd.DataFrame.from_dict(Tx(tx_obj))
+        pending.append(clean_tx, ignore_index=True)
         print(pending)
     except:
         print(e)
