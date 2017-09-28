77162745ba94b0deec4bcf0df1a025853f459e00
jake <jsbe70@protonmail.com>
2017-09-27 20:32:43 -0400

    p

--- gasStation.py
+++ gasStation.py
@@ -7,18 +7,24 @@ engine = create_engine(
     'mysql+mysqlconnector://ethgas:station@127.0.0.1:3306/tx', echo=False)
 
 def new_block_callback(block_hash):
-    print (block_hash)
+    try:
+        block_obj = web3.eth.getBlock(block_hash, True)
+        print(block_obj)
+    except:
+        print(e)
 
 def new_tx_callback(tx_hash):
-    print ('tx '+ tx_hash)
+    print (tx_hash)
 
-block_filter = web3.eth.filter('latest')
-tx_filter = web3.eth.filter('pending')
+    
+def filter():
+    block_filter = web3.eth.filter('latest')
+    tx_filter = web3.eth.filter('pending')
+    while True:
+        block_filter.watch(new_block_callback)
+        tx_filter.watch(new_tx_callback)
+        response = input("type q to quit \n")
+        if response == 'q':
+            break
 
-while True:
-    block_filter.watch(new_block_callback)
-    tx_filter.watch(new_tx_callback)
-
-    response = input("type q to quit /n")
-    if response == 'q':
-        break
+filter()
