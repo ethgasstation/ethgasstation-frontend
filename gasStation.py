1d87ab833eca8a12eba0a3bf34d1f4e871a03ce8
jake <jsbe70@protonmail.com>
2017-09-27 21:46:04 -0400

    p

--- gasStation.py
+++ gasStation.py
@@ -8,11 +8,10 @@ from web3 import Web3, HTTPProvider
 from sqlalchemy import create_engine
 
 web3 = Web3(HTTPProvider('http://localhost:8545'))
-
 engine = create_engine(
     'mysql+mysqlconnector://ethgas:station@127.0.0.1:3306/tx', echo=False)
 
-def round_gp_gwei(gp):
+def round_gp_10gwei(gp):
     """Rounds the gas price to gwei"""
     gp = gp/1e8
     if gp >=1 and gp<10:
@@ -36,24 +35,21 @@ class Tx ():
         self.ts = time.time()
         self.gp = tx_obj['gasPrice']
         self.gas_offered = tx_obj['gas']
-        self.gp_gwei = round_gp_gwei(tx_obj['gasPrice'])
-
-    
-
-
+        self.gp_10gwei = round_gp_10gwei(tx_obj['gasPrice'])
 
 
 def new_tx_callback(tx_hash):
     try:
         tx_obj = web3.eth.getTransaction(tx_hash)
         clean_tx = Tx(tx_obj)
-        print(clean_tx.hash)
-        print(clean_tx.gp_gwei)
+        pending = pending.append(clean_tx)
+        print(pending)
     except:
         print(e)
 
 
 def filter():
+    pending = pd.DataFrame(columns = ['hash', 'block_posted', 'to_address', 'from_address', 'ts', 'gp', 'gas_offered', 'gp_10gwei'])
     tx_filter = web3.eth.filter('pending')
     while True:
         tx_filter.watch(new_tx_callback)
