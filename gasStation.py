362b32037d9705d4d0ac2abef526dff38d6e525b
jake <jsbe70@protonmail.com>
2017-09-27 20:15:06 -0400

    p

--- gasStation.py
+++ gasStation.py
@@ -7,11 +7,13 @@ engine = create_engine(
     'mysql+mysqlconnector://ethgas:station@127.0.0.1:3306/tx', echo=False)
 
 def new_block_callback(block_hash):
-    print('hi')
-    print ("New Block: {0}").format(block_hash)
+    print (block_hash)
 
 new_block_filter = web3.eth.filter('latest')
 
+
 while True:
     new_block_filter.watch(new_block_callback)
-    
+    response = input("type q to quit")
+    if response == q:
+        break
