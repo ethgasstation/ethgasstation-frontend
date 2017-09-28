ce1cc5d5a88e5bfbf2df1a5b991fb90f8aa17668
jake <jsbe70@protonmail.com>
2017-09-27 21:07:25 -0400

    p

--- gasStation.py
+++ gasStation.py
@@ -1,3 +1,4 @@
+import time
 from web3 import Web3, HTTPProvider
 from sqlalchemy import create_engine
 
@@ -6,22 +7,36 @@ web3 = Web3(HTTPProvider('http://localhost:8545'))
 engine = create_engine(
     'mysql+mysqlconnector://ethgas:station@127.0.0.1:3306/tx', echo=False)
 
-def new_block_callback(block_hash):
+
+
+
+class Tx ():
+    def __init__(self, tx_obj):
+        self.hash = tx_obj.hash
+        self.block_posted = tx_obj.blockNumber
+        self.to_address = tx_obj.to
+        self.from_address = tx_obj.from
+        self.ts = time.time()
+        self.gp = tx_obj.gasPrice/1e6
+        self.gas_offered = tx_obj.gas
+
+    
+
+
+
+
+def new_tx_callback(tx_hash):
     try:
-        block_obj = web3.eth.getBlock(block_hash, True)
-        print(block_obj)
+        tx_obj = web3.eth.getTransactions(tx_hash)
+        clean_tx = Tx(tx_obj)
+        print(clean_tx)
     except:
         print(e)
 
-def new_tx_callback(tx_hash):
-    print (tx_hash)
 
-    
 def filter():
-    block_filter = web3.eth.filter('latest')
     tx_filter = web3.eth.filter('pending')
     while True:
-        block_filter.watch(new_block_callback)
         tx_filter.watch(new_tx_callback)
         response = input("type q to quit \n")
         if response == 'q':
