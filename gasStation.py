3139a4aaafe79615c276f68f0df82de36064fe4c
jake <jsbe70@protonmail.com>
2017-09-27 21:17:53 -0400

    p

--- gasStation.py
+++ gasStation.py
@@ -1,4 +1,9 @@
 import time
+import sys
+import json
+import urllib
+import pandas as pd
+import numpy as np
 from web3 import Web3, HTTPProvider
 from sqlalchemy import create_engine
 
@@ -7,6 +12,18 @@ web3 = Web3(HTTPProvider('http://localhost:8545'))
 engine = create_engine(
     'mysql+mysqlconnector://ethgas:station@127.0.0.1:3306/tx', echo=False)
 
+def round_gp_gwei(gp):
+    """Rounds the gas price to gwei"""
+    gp = gp/1e8
+    if gp >=1 and gasPrice<10:
+        gp = np.floor(gasPrice)
+    elif gp>=10
+        gp=gp/10
+        gp=np.floor(gasPrice)
+        gp=gp*10
+    else:
+        gp = 0
+    return gp
 
 
 
@@ -17,8 +34,9 @@ class Tx ():
         self.to_address = tx_obj['to']
         self.from_address = tx_obj['from']
         self.ts = time.time()
-        self.gp = tx_obj['gasPrice']/1e6
+        self.gp = tx_obj['gasPrice']
         self.gas_offered = tx_obj['gas']
+        self.gp_gwei = round_gp_gwei(tx_obj['gasPrice'])
 
     
 
@@ -29,7 +47,8 @@ def new_tx_callback(tx_hash):
     try:
         tx_obj = web3.eth.getTransaction(tx_hash)
         clean_tx = Tx(tx_obj)
-        print(clean_tx.gp)
+
+        print(clean_tx.gp_gwei)
     except:
         print(e)
 
