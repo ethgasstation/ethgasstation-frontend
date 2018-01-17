f62e97ccfe8051d25a13594c9563ccc896a1b9df
10a7 <10a7@users.noreply.github.com>
2018-01-16 19:10:41 -0800

    harden json file handling

--- build/php/common.php
+++ build/php/common.php
@@ -20,9 +20,20 @@ define('EGS_DESCRIPTION', 'Unofficial ethgasstation.info node run by a volunteer
 // fast dirty convenience functions
 // no point in abstraction, all of this is going away
 function path_to_json(string $json_file) {
-    return realpath(JSON_LOCATION . '/' . $json_file);
+    $path = JSON_LOCATION . '/' . $json_file;
+    if (file_exists($path)) {
+        return realpath(JSON_LOCATION . '/' . $json_file);
+    } else {
+        return false;
+    }
 }
 
 function get_json_file(string $json_file_name) {
-    return file_get_contents(path_to_json($json_file_name));
+    $path = path_to_json($json_file_name);
+    if ($path) {
+        return file_get_contents(path_to_json($json_file_name));
+    } else {
+        error_log("Cannot find file: " . $json_file_name);
+        return "";
+    }
 }
