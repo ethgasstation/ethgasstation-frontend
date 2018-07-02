854f8910a2cad94713054be1f19f8ae9d4751ea2
egs <ethgasstation@protonmail.com>
2018-07-01 22:01:59 -0400

    refactor

--- txPoolReport.php
+++ txPoolReport.php
@@ -138,7 +138,7 @@
                         
                         echo('</tr>');}
 
-                      }
+                      
                       ?>
                         </tbody>
                     </table>
