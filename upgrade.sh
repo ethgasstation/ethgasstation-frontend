890980c0e30590f9b1c18154e634a04f87146aa5
asmodat <asmodat@gmail.com>
2019-02-01 19:57:28 +0100

    frontend upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,7 @@ set -e
 # before starting this script:
 # cd /usr/local/SettleFinance/ethgasstation-frontend
 # git pull
+# chmod -R 777 /usr/local/SettleFinance/ethgasstation-frontend/upgrade.sh
 
 echo "####################################"
 echo "# ETH GAS STARTION FRONEND UPGRADE #"
@@ -14,8 +15,32 @@ rm -v /usr/local/SettleFinance/common.php || echo "Backup common file was probab
 
 cp /var/www/ethgasstation.settle.host/public_html/build/php/common.php /usr/local/SettleFinance/common.php
 
-#cp -v /usr/local/ethgasstation-backend/settings.conf /usr/local/ethgasstation-backend-settings.conf
-#cp -v -r /usr/local/SettleFinance/ethgasstation-frontend/* /var/www/ethgasstation.settle.host/public_html/
+echo "Stopping Apache..."
+apachectl stop
+
+rm -r -f -v /var/www/ethgasstation.settle.host/public_html/*
+
+cp -v -r /usr/local/SettleFinance/ethgasstation-frontend/* /var/www/ethgasstation.settle.host/public_html/
+
+rm -f -v /var/www/ethgasstation.settle.host/public_html/build/php/common.php
+
+cp -v /usr/local/SettleFinance/common.php /var/www/ethgasstation.settle.host/public_html/build/php/common.php
+
+mkdir -p -v /var/www/ethgasstation.settle.host/public_html/json
+
+chmod -R 777 /var/www/ethgasstation.settle.host/public_html/json
+
+echo "Startting Apache..."
+
+/etc/init.d/apache2 start
+/etc/init.d/apache2 reload
+
+echo "Restarting Backend..."
+
+systemctl restart ethgassbackend
+
+
+
 
 
 
