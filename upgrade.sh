9b841b3454d120d714cd7b160fa18ba8f67d2f19
asmodat <asmodat@gmail.com>
2019-02-01 20:49:42 +0100

    fixed missing json folder

--- upgrade.sh
+++ upgrade.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 set -e
 
-# before starting this script:
-# cd /usr/local/SettleFinance/ethgasstation-frontend && git fetch --all && git reset --hard origin/master && git pull && chmod -R 777 /usr/local/SettleFinance/ethgasstation-frontend/upgrade.sh
+# Run following command to execute the script:
+# cd /usr/local/SettleFinance/ethgasstation-frontend && git fetch --all && git reset --hard origin/master && git pull && chmod -R 777 /usr/local/SettleFinance/ethgasstation-frontend/upgrade.sh && ./upgrade.sh
 
 echo "####################################"
 echo "# ETH GAS STARTION FRONEND UPGRADE #"
 echo "####################################"
 
-echo "Stopping Apache and Backend..."
-systemctl stop apache2
-systemctl stop ethgassbackend
 
 rm -v /usr/local/SettleFinance/common.php || echo "Backup common file was probably already removed.";
 rm -r -f -v /usr/local/SettleFinance/json || echo "Backup json files were probably already removed.";
 
+mkdir -p -v /var/www/ethgasstation.settle.host/public_html/json
+mkdir -p -v /usr/local/SettleFinance/json
 cp /var/www/ethgasstation.settle.host/public_html/build/php/common.php /usr/local/SettleFinance/common.php
 cp /var/www/ethgasstation.settle.host/public_html/json/* /usr/local/SettleFinance/json
+
+echo "Stopping Apache and Backend..."
+systemctl stop apache2
+systemctl stop ethgassbackend
 
 rm -r -f -v /var/www/ethgasstation.settle.host/public_html/*
 
@@ -26,7 +29,6 @@ rm -f -v /var/www/ethgasstation.settle.host/public_html/build/php/common.php
 
 cp -v /usr/local/SettleFinance/common.php /var/www/ethgasstation.settle.host/public_html/build/php/common.php
 
-mkdir -p -v /var/www/ethgasstation.settle.host/public_html/json
 cp /usr/local/SettleFinance/json/* /var/www/ethgasstation.settle.host/public_html/json
 
 chmod -R 777 /var/www/ethgasstation.settle.host/public_html/json
