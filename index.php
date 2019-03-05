14471456b29c747c67328357f70500a40eaf8e09
arman37 <arman.it37@gmail.com>
2019-03-06 00:31:41 +0600

    Update class name

--- index.php
+++ index.php
@@ -284,7 +284,7 @@
                       <h4>Real Time Gas Use: <small> Block Limit (last 10)</small></h4>
                       <div class="clearfix"></div>
                     </div>
-                    <div class="x_content speedometer">
+                    <div class="x_content sMeter">
                         <div id="speedometer" class="speedometer"></div>
                         <p id="blockNum">Last Block: </p> 
                     </div>
