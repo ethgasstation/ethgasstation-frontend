63485da900ce86fac91f569d291b73968ec3b746
arman37 <arman.it37@gmail.com>
2019-05-20 22:16:49 +0600

    Show top 25 projects on leaderboard

---
+++ topProjects.php
@@ -0,0 +1,30 @@
+<?php 
+
+  /**
+  *
+  * @author arman
+  * @since 5/20/19
+  */
+
+  $ch = curl_init();
+  $url = 'http://gasburners-jsapi-prod.us-west-1.elasticbeanstalk.com/api/leaderboard';
+
+  curl_setopt_array(
+  $ch, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true
+  ));
+
+  $output = curl_exec($ch);
+  $decodedData = json_decode($output);
+  $counter = 1;
+
+  foreach($decodedData as $data) {
+    if ($data->project == "unknown") {
+      echo "<tr><td>". $counter++ .".</td><td>". $data->contracts[0]->contract ."</td><td>". $data->gas ."</td><td>3.4%</td><td>4,234</td><td>1234</td></tr>";
+    } else {
+      echo "<tr><td>". $counter++ .".</td><td>". $data->project ."</td><td>". $data->gas ."</td><td>3.4%</td><td>4,234</td><td>1234</td></tr>";
+    }
+  }
+ 
+?>
