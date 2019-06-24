b445aab6792f9e35ce50b9cbcd294b2341e4dcf3
arman37 <arman.it37@gmail.com>
2019-06-25 02:15:14 +0600

    Reposition footer

--- index.php
+++ index.php
@@ -364,21 +364,23 @@
             </div>
           <!-- 4th row ends -->
           </div>
-          <!-- footer content -->
-            <footer>
-              <div class='eth_footer_links'>
-                <a href="https://docs.ethgasstation.info" target="_blank">API</a>
-                <a href="https://ethgasstation.info/feedback.html" target="_blank">Feedback</a>
-                <a href="https://twitter.com/ethgasstation" target="_blank">Twitter</a>
-                <a href="https://discord.gg/mzDxADE" target="_blank">Discord</a>
-              </div>
-           
-              <div class="concourse_link"><a href="https://concourseopen.com" target="_blank">Concourse Open Construction</a></div>
-              <div class="clearfix"></div>
-            </footer>
-          <!-- /footer content -->
         </div>           
       <!-- /page content ends -->
+
+      <!-- footer content -->
+        <footer>
+          <div class='eth_footer_links'>
+            <a href="https://docs.ethgasstation.info" target="_blank">API</a>
+            <a href="https://ethgasstation.info/feedback.html" target="_blank">Feedback</a>
+            <a href="https://twitter.com/ethgasstation" target="_blank">Twitter</a>
+            <a href="https://discord.gg/mzDxADE" target="_blank">Discord</a>
+          </div>
+       
+          <div class="concourse_link"><a href="https://concourseopen.com" target="_blank">Concourse Open Construction</a></div>
+          <div class="clearfix"></div>
+        </footer>
+      <!-- /footer content -->
+          
       </div>
     </div>
 
