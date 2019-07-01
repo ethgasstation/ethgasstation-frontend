4536729def60253da6c53bc03e653c3ce78acaa0
arman37 <arman.it37@gmail.com>
2019-07-02 04:52:57 +0600

    Shorten contracts and make them clickable

--- topProjects.php
+++ topProjects.php
@@ -9,6 +9,10 @@
   $ch = curl_init();
   $url = 'http://gasburners-jsapi-prod.us-west-1.elasticbeanstalk.com/api/leaderboard';
 
+  function _shortenContract($contract) {
+    return substr($contract, 0, 6) . '...' . substr($contract, strlen($contract) - 4);
+  }
+
   curl_setopt_array(
   $ch, array(
     CURLOPT_URL => $url,
@@ -20,8 +24,10 @@
   $counter = 1;
 
   foreach($decodedData as $data) {
+    $etherscanLink = "https://etherscan.io/address/" . $data->contract;
+
     if ($data->project == "unknown") {
-      echo "<tr><td>". $counter++ .".</td><td>". $data->contract ."</td><td>". $data->costETH ."</td><td>". $data->avgGwei ."</td><td>$". $data->costUsd ."</td></tr>";
+      echo "<tr><td>" . $counter++ . ".</td><td><a href=" . $etherscanLink . " class='etherscan_link' target='_blank'>" . _shortenContract($data->contract) . "</a></td><td>". $data->costETH . "</td><td>" . $data->avgGwei . "</td><td>$" . $data->costUsd . "</td></tr>";
     } else {
       echo "<tr><td>". $counter++ .".</td><td>". $data->project ."</td><td>". $data->costETH ."</td><td>". $data->avgGwei ."</td><td>$". $data->costUsd ."</td></tr>";
     }
