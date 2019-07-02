b9387b14a4d7c022000707d726b5640b81050a8d
arman37 <arman.it37@gmail.com>
2019-07-02 21:03:30 +0600

    Change dom structure and add transparent icon

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
           <div class="left_col scroll-view">
 
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.php" class="site_title"><img src="images/ETHgas.png" style="height:32px;width:32px"> <span><?php echo EGS_TITLE; ?></span></a>
+              <a href="index.php" class="site_title"><img src="images/egs_transparent.png" class="egs_icon"></a>
             </div>
             
             <div class="clearfix"></div>
@@ -12,20 +12,18 @@
 
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-                <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a href="index.php"><i class ="fa fa-home"></i>Main Page</a></li>
-                  <li><a href="calculatorTxV.php"><i class ="fa fa-calculator"></i>Tx Calculator</a></li>
-                  <li><a href="txPoolReport.php"><i class ="fa fa-eye"></i>TxPool Vision</a></li>
+                  <li><a href="calculatorTxV.php">Tx Calculator</a></li>
+                  <li><a href="txPoolReport.php">TxPool Vision</a></li>
                   <!--<li><a href="minerTable.php"><i class ="fa fa-cubes"></i>Miner Stats</a></li>
                   <li><a href="pendingTxReport.php"><i class ="fa fa-flag"></i>Pending Tx Report</a></li>
                   <li><a href="hashPowerTable.php"><i class ="fa fa-cubes"></i>Hash Power by Gas Price</a></li>
                   <li><a href="safeLow.php"><i class ="fa fa-clock-o"></i>Safe Low</a></li>
                   <li><a href="txpool2.php"><i class ="fa fa-upload"></i>TxPool Monitor</a></li>
                   <li><a href="predictionTable.php"><i class ="fa fa-cubes"></i>Predict Confirm Time</a></li>-->
-                  <li><a href="validatedTable.php"><i class ="fa fa-cubes"></i>Low Gas Price Watch List</a></li>
-                  <li><a href="gasguzzlers.php"><i class ="fa fa-car"></i>Gas Burners</a></li>
-                  <li><a><i class="fa fa-bookmark-o"></i>FAQ<span class="fa fa-chevron-down"></span></a>
+                  <li><a href="validatedTable.php">Low Gas Price Watch List</a></li>
+                  <li><a href="gasguzzlers.php">Gas Burners</a></li>
+                  <li><a>FAQ<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="about.php">What Is This Site?</a></li>
                       <li><a href="FAQcalc.php">Calculator</a></li>
@@ -33,7 +31,7 @@
                       <li><a href="FAQpage.php">Other FAQ</a></li>
                     </ul>
                   </li>
-                  <li><a><i class="fa fa-link"></i> External Links <span class="fa fa-chevron-down"></span></a>
+                  <li><a>Links <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="https://coincenter.org/entry/what-is-ethereum" target="_blank">What is Ethereum?</a></li>
                       <li><a href="https://www.ethereum.org/ether" target="_blank">Ethereum FAQ</a></li>
@@ -42,10 +40,10 @@
                     </ul>
                   </li>
                   <li>
-                    <a href="https://docs.ethgasstation.info/"><i class ="fa fa-book"></i>API</a>
+                    <a href="https://docs.ethgasstation.info/">API</a>
                   </li>
                   <li>
-                    <a href="/feedback.html"><i class ="fa fa-envelope"></i>Feedback</a>
+                    <a href="/feedback.html">Feedback</a>
                   </li>
                 </ul>
               </div>
