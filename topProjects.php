596c1d682313f11cbb0cf057be1aae70f4fe4a78
arman37 <arman.it37@gmail.com>
2019-07-03 03:34:45 +0600

    Show spam projects with spam reference link

--- topProjects.php
+++ topProjects.php
@@ -25,11 +25,15 @@
 
   foreach($decodedData as $data) {
     $etherscanLink = "https://etherscan.io/address/" . $data->contract;
+    $spamLink = '';
+    if ($data->isSpam) {
+      $spamLink = "<a href=" . $data->spamRefLink . " class='spam_reference_link' target='_blank'>(Scam Reported)</a>";
+    }
 
     if ($data->project == "unknown") {
-      echo "<tr><td>" . $counter++ . ".</td><td><a href=" . $etherscanLink . " class='etherscan_link' target='_blank'>" . _shortenContract($data->contract) . "</a></td><td>". $data->costETH . "</td><td>" . $data->avgGwei . "</td><td>$" . $data->costUsd . "</td></tr>";
+      echo "<tr><td>" . $counter++ . ".</td><td><a href=" . $etherscanLink . " class='etherscan_link' target='_blank'>" . _shortenContract($data->contract) . $spamLink . "</a></td><td>". $data->costETH . "</td><td>" . $data->avgGwei . "</td><td>$" . $data->costUsd . "</td></tr>";
     } else {
-      echo "<tr><td>". $counter++ .".</td><td>". $data->project ."</td><td>". $data->costETH ."</td><td>". $data->avgGwei ."</td><td>$". $data->costUsd ."</td></tr>";
+      echo "<tr><td>". $counter++ .".</td><td>" . $data->project . $spamLink . "</td><td>". $data->costETH ."</td><td>". $data->avgGwei ."</td><td>$". $data->costUsd ."</td></tr>";
     }
   }
  
