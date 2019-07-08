d06354657166754dc39096ef0cdc390449e185e9
arman37 <arman.it37@gmail.com>
2019-07-09 03:34:48 +0600

    Reset custom css url

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <link href="build/css/custom.css?d=13" rel="stylesheet">
+    <link href="build/css/custom.css?d=14" rel="stylesheet">
 	
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-134672026-1"></script>
