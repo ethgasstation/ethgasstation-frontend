dfafb0457137207d64c1322707bd48fb014fd8b0
arman37 <arman.it37@gmail.com>
2019-07-16 00:04:37 +0600

    Set style for boxes

--- index.php
+++ index.php
@@ -130,7 +130,7 @@
                     <div class="text-container">
                       <div class="count_top"><span>fast</span><span>< 2m</span></div>
                       <div class="count_top usd">
-                        <?php $fee = round($gpRecs2['fast']*21000/1e9*$exchangeRate/10, 3); echo($currString . $fee . '/transfer'); ?>
+                        <?php $fee = round($gpRecs2['fast']*21000/1e9*$exchangeRate/10, 3); echo($currString . $fee . ' / transfer'); ?>
                       </div>
                     </div>
                   </div>
@@ -145,7 +145,7 @@
                     <div class="text-container">
                       <div class="count_top"><span>standard</span><span>< 5m</span></div>
                       <div class="count_top usd">
-                        <?php $fee = round($gpRecs2['average']*21000/1e9*$exchangeRate/10, 3); echo($currString . $fee . '/transfer'); ?>
+                        <?php $fee = round($gpRecs2['average']*21000/1e9*$exchangeRate/10, 3); echo($currString . $fee . ' / transfer'); ?>
                       </div>
                     </div>
                   </div>
@@ -160,7 +160,7 @@
                     <div class="text-container">
                       <div class="count_top"><span>safe low</span><span>< 30m</span></div>
                       <div class="count_top usd">
-                        <?php $fee = round($gpRecs2['safeLow']*21000/1e9*$exchangeRate/10, 3); echo($currString . $fee . '/transfer'); ?>
+                        <?php $fee = round($gpRecs2['safeLow']*21000/1e9*$exchangeRate/10, 3); echo($currString . $fee . ' / transfer'); ?>
                       </div>
                     </div>
                   </div>
