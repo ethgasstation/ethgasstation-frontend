6989a47b2847cdae04341ac76aae0ff09f7c9995
arman37 <arman.it37@gmail.com>
2019-07-17 04:54:00 +0600

    Update egs header design for mobile

--- index.php
+++ index.php
@@ -88,18 +88,19 @@
               <div class="header_left_contents">
                 <div  class="social_links">
                   <div class="social_link">
-                      <a href="https://discord.gg/mzDxADE" target="_blank">
-                        <img class="social_icon" src="/images/discord.svg"></a>
+                      <a href="https://discord.gg/mzDxADE" target="_blank"><img class="social_icon primary" src="/images/discord.svg"></a>
+                      <a href="https://discord.gg/mzDxADE" target="_blank"><img class="social_icon secondary" src="/images/discord-green.svg"></a>
                   </div>
                   <div class="social_link">
-                      <a href="https://twitter.com/ethgasstation" target="_blank"><img class="social_icon" src="/images/twitter.svg"></a>
+                      <a href="https://twitter.com/ethgasstation" target="_blank"><img class="social_icon primary" src="/images/twitter.svg"></a>
+                      <a href="https://twitter.com/ethgasstation" target="_blank"><img class="social_icon secondary" src="/images/twitter-green.svg"></a>
                   </div>
                 </div>
               </div>
 
               <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                  <a href="#" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-globe"></i><span class="change_currency_text"> Change Currency</span>
+                  <a href="#" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-globe"></i><span class="change_currency_text"> Change Currency <img class='down_arrow' src='/images/down-arrow.svg'></span>
                   </a>
                   <ul class="dropdown-menu">
                     <li id="usd"><a href="#"> USD<?php if($currency=='usd'){echo'<span class="pull-right"><i class="fa fa-check"></i></span>';}?></a></li>
