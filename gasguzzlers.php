a9f8aefe45ddb8cf06bc42918b540842ec198838
arman37 <arman.it37@gmail.com>
2019-07-31 00:58:10 +0600

    Change page width

--- gasguzzlers.php
+++ gasguzzlers.php
@@ -89,7 +89,7 @@
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="row">
-              <div class="col-md-8 col-sm-8 col-xs-12">
+              <div class="col-md-12 col-sm-12 col-xs-12" style="max-width: 1200px;">
                 <div class="x_panel tile fixed_height_420">
                   <div class="x_title">
                     <h4>Top 10 ETH Contracts By Transaction Count Over Last 1,500 Blocks</h4>
--- txPoolReport.php
+++ txPoolReport.php
@@ -87,7 +87,7 @@
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="row">
-              <div class="col-md-8 col-sm-12 col-xs-12">
+              <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel tile fixed_height_420">
                   <div class="x_title">
                     <h4>Txpool Data At Block <span style = 'color:#1ABB9C'><?php echo $gpRecs2['blockNum']?></h4>
